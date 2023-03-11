f14810981534ff1b5e544e3fc36b808ab87ebc65
0xyukine <nesryn@protonmail.com>
2023-03-11 02:47:53 +0000

    Tidied up and removed duplicate methods

--- src/furREST.py
+++ src/furREST.py
@@ -11,29 +11,32 @@ class FurREST:
 		self.session.auth = (username, api_key)
 		self.session.headers.update({'User-Agent':'furREST wrapper - yukine.moe'})
 
-	def search_raw(self, search, page_limit, post_limit):
-		return methods.getmethods.search_posts(self.session, search, page_limit, post_limit)
+	def search(self, search="", page=1, post_limit=75, raw=False):	
+		tags = '+'.join(search)
+		params = f"?tags={tags}&page={str(page)}&limit={post_limit}"
+		if raw == True:
+			return methods.getmethods.search_posts(self.session, params)
+		elif raw == False:
+			posts = []
+			for post in methods.getmethods.search_posts(self.session, params)["posts"]:
+				posts.append(structs.post.Post(post))
+			return posts
 
-	def search(self, search, page_limit, post_limit):
-		posts = []
-		for post in methods.getmethods.search_posts(self.session, search, page_limit, post_limit)["posts"]:
-			posts.append(structs.post.Post(post))
-		return posts
+	def favorites(self, page=1, post_limit=75, raw=False):
+		params = f"?page={str(page)}&limit={post_limit}"
+		if raw == True:
+			return methods.getmethods.search_favorites(self.session, params)
+		elif raw == False:
+			posts = []
+			for post in methods.getmethods.search_favorites(self.session, params)["posts"]:
+				posts.append(structs.post.Post(post))
+			return posts
 
-	def favorites_raw(self):
-		return methods.getmethods.search_favorites(self.session)
-
-	def favorites(self):
-		posts = []
-		for post in methods.getmethods.search_favorites(self.session)["posts"]:
-			posts.append(structs.post.Post(post))
-		return posts
-
-	def get_profile_raw(self, user):
-		return methods.getmethods.get_profile(self.session, user)
-
-	def get_profile(self, user):
-		return structs.profile.ProfileOther(methods.getmethods.get_profile(self.session, user))
+	def get_profile(self, user, raw=False):
+		if raw == True:
+			return methods.getmethods.get_profile(self.session, user)
+		elif raw == False:
+			return structs.profile.ProfileOther(methods.getmethods.get_profile(self.session, user))
 
 	def profile(self):
 		return structs.profile.ProfileSelf(methods.getmethods.get_profile(self.session, self.username))
--- src/methods/getmethods.py
+++ src/methods/getmethods.py
@@ -1,10 +1,10 @@
 import requests
 
-def search_posts(session, search, page, post_limit):
-	return session.get("https://e621.net/posts.json?page={}&tags={}&limit={}".format(page, search, post_limit)).json()
+def search_posts(session, params):
+	return session.get(f"https://e621.net/posts.json{params}").json()
 
-def search_favorites(session):
-	return session.get("https://e621.net/favorites.json").json()
+def search_favorites(session, params):
+	return session.get(f"https://e621.net/favorites.json{params}").json()
 
 def get_profile(session, user_id):
-	return session.get("https://e621.net/users/{}.json".format(user_id)).json()
+	return session.get(f"https://e621.net/users/{user_id}.json").json()
