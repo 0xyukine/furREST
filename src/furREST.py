d4fd2f8f2fe4c6d7a198af080eb7a375f3395c70
0xyukine <nesryn@protonmail.com>
2023-03-18 03:42:52 +0000

    Commenting code following numpy docstring notation

--- src/furREST.py
+++ src/furREST.py
@@ -14,7 +14,29 @@ class FurREST:
 		self.session.auth = (username, api_key)
 		self.session.headers.update({'User-Agent':'furREST wrapper - yukine.moe'})
 
-	def search(self, search="", page=1, post_limit=75, raw=False):	
+	def search(self, search="", page=1, post_limit=75, raw=False):
+		"""
+		Returns posts with optional search parameters
+
+		Parameters
+		----------
+		search : str or list
+			Single tag or a list of tags to be searched
+		page : int, default=1
+			The page of the search results to be returned
+		post_limiit : int, default=75
+			The number of posts to be returned per page
+		raw : bool, default=False
+			Whether to return as a list of class instances or the raw json return
+
+		Returns
+		-------
+		posts (formatted) : list
+			A list of post class instances
+		posts (raw) : dict
+			A list of posts in json form
+		"""
+
 		tags = '+'.join(search)
 		params = f"?tags={tags}&page={str(page)}&limit={post_limit}"
 		if raw == True:
@@ -26,6 +48,26 @@ class FurREST:
 			return posts
 
 	def favorites(self, page=1, post_limit=75, raw=False):
+		"""
+		Returns the user's favourited posts
+
+		Parameters
+		----------
+		page : int, default=1
+			The page of the searh results to be returned
+		post_limit : int, default=75
+			The number of posts to be returned per page
+		raw : bool, default=False
+			Whether to return as a list of class instances or the raw json return
+
+		Returns
+		-------
+		posts (formatted) : list
+			A list of post class instances
+		posts (raw) : dict
+			A list of posts in json form
+		"""
+
 		params = f"?page={str(page)}&limit={post_limit}"
 		if raw == True:
 			return methods.getmethods.search_favorites(self.session, params)
@@ -36,15 +78,79 @@ class FurREST:
 			return posts
 
 	def get_profile(self, user, raw=False):
+		"""
+		Returns the specified user's profile
+
+		Parameters
+		----------
+		user : int or str
+			The ID or username of the user who's profile you want to get
+		raw: bool, default=False
+			Whether to return the profile as a json or as profile class instance
+
+		Returns
+		-------
+		profile (formatted) : profile instance
+			An instance of the profile class with the relevant user information
+		profile (raw) : dict
+			The json returned by the request
+
+		"""
+
 		if raw == True:
 			return methods.getmethods.get_profile(self.session, user)
 		elif raw == False:
 			return structs.profile.ProfileOther(methods.getmethods.get_profile(self.session, user))
 
 	def profile(self):
+		"""
+		Returns the profile of the connected user
+
+		Returns
+		-------
+		profile : profile instance
+			An instance of the profile class containing all of the user's information
+		"""
+
 		return structs.profile.ProfileSelf(methods.getmethods.get_profile(self.session, self.username))
 
-	def get_pools(self, name="", pool_id="", desc="", creator="", creator_id="", active="", category="", order="", limit=75, raw=False):
+	def get_pools(self, name="", pool_id="", desc="", creator="", creator_id="", active="", category="", order="", page=1, limit=75, raw=False):
+		"""
+		Returns pools with optional search parameters
+
+		Parameters
+		----------
+		name : str
+			Match name of pools
+		pool_id : int
+			Match pools with id
+		desc : str
+			Match pools with description
+		creator : str
+			Match pools by creator name
+		creator : int
+			Match pools by creator id
+		active : bool
+			Match pools by active or not
+		category : {"series", "collection"}
+			Match pools by their category
+		order : {"name", "created_at", "updated_at", "post_count"}
+			Sort pools by a specified order
+		page : int, default=1
+			Page of results to be returned
+		limit : int, default=75
+			Number of pools to be returned per search
+		raw : bool, default=False
+			Whether to return as a list of class instances or the raw json return
+
+		Returns
+		-------
+		pools (formatted) : list
+			A list of of pool class instances
+		pools (raw) : dict
+			A list of pools in json form
+		"""
+
 		params = "?"\
 			f"search[name_matches]={name}&"\
 			f"search[id]={pool_id}&"\
@@ -54,6 +160,7 @@ class FurREST:
 			f"search[is_active]={active}&"\
 			f"search[category]={category}&"\
 			f"search[order]={order}&"\
+			f"page={page}&"\
 			f"limit={limit}"
 
 		print("Getting pools: " + params)
@@ -66,7 +173,39 @@ class FurREST:
 				pools.append(structs.pool.Pool(pool))
 			return pools
 
-	def get_sets(self, name="", set_id="", shortname="", creator="", creator_id="", order="", limit=75, raw=False):
+	def get_sets(self, name="", set_id="", shortname="", creator="", creator_id="", order="", page=1, limit=75, raw=False):
+		"""
+		Returns sets with optional search parameters
+
+		Parameters
+		----------
+		name : str
+			Match name of sets
+		set_id : int
+			Match sets with id
+		shortname : str
+			Match sets by shortname
+		creator : str
+			Match sets by creator name
+		creator : int
+			Match sets by creator id
+		order : {"name", "shortname" "created_at", "updated_at", "post_count"}
+			Sort sets by a specified order
+		page : int, default=1
+			Page of results to be returned
+		limit : int, default=75
+			Number of sets to be returned per search
+		raw : bool, default=False
+			Whether to return as a list of class instances or the raw json return
+
+		Returns
+		-------
+		sets (formatted) : list
+			A list of of pool class instances
+		sets (raw) : dict
+			A list of pools in json form
+		"""
+
 		params = "?"\
 			f"search[name]={name}&"\
 			f"search[id]={set_id}&"\
@@ -74,6 +213,7 @@ class FurREST:
 			f"search[order]={order}&"\
 			f"search[creator_name]={creator}&"\
 			f"search[creator_id]={creator_id}&"\
+			f"page={page}&"\
 			f"limit={limit}"
 
 		print("Getting sets: " + params)
@@ -87,6 +227,26 @@ class FurREST:
 			return sets
 
 	def get_popular(self, date=time.gmtime(), scale="day", raw=False):
+		"""
+		Returns popular posts for a given date and timescale
+
+		Parameters
+		----------
+		date : time.date, default=system's current date
+			The date to retrieve popular posts from
+		scale : {"day", "week", "month"}
+			The daterange to retrieve popular posts from
+		raw : bool, default=False
+			Whether to return as a list of class instances or the raw json return
+
+		Returns
+		-------
+		posts (formatted) : list
+			A list of post class instances
+		posts (raw) : dict
+			A list of posts in json form
+		"""
+
 		params = f"?date={time.strftime('%Y-%m-%d',date)}&scale={scale}"
 
 		if raw == True:
